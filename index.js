4de949d7ef63eb0ec945cd6842b0be30f9f682e3
Brigham <brig.johnson@gmail.com>
2022-05-04 21:36:12 -0400

    commit tree 1f98c8c5bd7d259f27431c6a24796a6cd8d2723e

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your code here!
+// remove the <main> with id 'main' (main#main)
+const elem1 = document.querySelector('main#main');
+elem1.remove();
+// console.log(elem1)
+// const bodyEl = document.querySelector('body');
+// bodyEl.append(elem1);
+
+// has a 'newHeader' variable that points to node 'h1#victory'"
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+//console.log(newHeader);
+//console.log(newHeader.nodeName);
+
+//"Make sure you create an <h1> with id 'victory'"
+//  expect(newHeader.nodeName, 
+
+// "has a 'newHeader' variable that points to node 'h1#victory'"
+//"Make sure you create an <h1> with id 'victory'").eql('victory')
+//  expect(newHeader.id, 
+
+newHeader.textContent = 'YOUR-NAME is the champion';
+console.log(newHeader);
+// "has a 'newHeader' variable that points to node 'h1#victory' with \"YOUR-NAME is the champion\" inside"
+// expect(newHeader.innerHTML
+// "Make sure you create an <h1> with id 'victory' with a sweet message in it").to.include("is the champion");
